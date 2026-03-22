ff18a49e01bad0432acdc31d638c38c266e874ae
Ander Vicario <andervicariozabala@gmail.com>
2026-03-22 23:44:24 +0100

    feat: customize system bar appearance and window styling
    - Implemented `setupWindow` in `MainActivity` to programmatically configure the status and navigation bar colors using the primary theme color.
    - Updated `activity_main.xml` to include a container ID and adjusted padding and `fitsSystemWindows` properties for better layout control.
    - Removed hardcoded status bar attributes from both light and dark `themes.xml` to favor programmatic configuration.
    - Updated project configuration for a new physical deployment target.

--- app/src/main/java/com/av19/netanalyzer/ui/main/MainActivity.java
+++ app/src/main/java/com/av19/netanalyzer/ui/main/MainActivity.java
@@ -1,8 +1,19 @@
 package com.av19.netanalyzer.ui.main;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.av19.netanalyzer.R;
@@ -10,6 +21,7 @@ import com.av19.netanalyzer.ui.devices.DevicesFragment;
 import com.av19.netanalyzer.ui.home.HomeFragment;
 import com.av19.netanalyzer.ui.settings.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.color.MaterialColors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupWindow();
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
@@ -40,6 +53,21 @@ public class MainActivity extends AppCompatActivity {
 
             return loadFragment(selectedFragment);
         });
+    }
+
+    private void setupWindow() {
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        Window window = getWindow();
+        WindowCompat.setDecorFitsSystemWindows(window, true);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.primary));
+        window.setNavigationBarColor(ContextCompat.getColor(this, R.color.primary));
     }
 
     private boolean loadFragment(Fragment fragment) {
