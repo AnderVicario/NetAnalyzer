dda8d552460c395e8de06e0f0f00628f7fda20c7
Ander Vicario <andervicariozabala@gmail.com>
2026-04-03 00:59:12 +0200

    fix: improve OS detection logic in FingerprintManager
    - Modified `FingerprintManager` to only apply TTL-based OS guessing if the device's OS field is not already populated.

--- app/src/main/java/com/av19/netanalyzer/utils/FingerprintManager.java
+++ app/src/main/java/com/av19/netanalyzer/utils/FingerprintManager.java
@@ -25,7 +25,9 @@ public class FingerprintManager {
     public DeviceInfo getDeviceInfo(NetworkInfo network, String ip, String mac, String vendor, List<Integer> openPorts, Integer ttl) {
         DeviceInfo device = new DeviceInfo(ip, mac, vendor, openPorts);
         device.setTtl(ttl);
-        device.setOs(guessOsFromTtl(ttl));
+        if (device.getOs()==null) {
+            device.setOs(guessOsFromTtl(ttl));
+        }
         if (ip.equals(network.getIp())) {
             device.setIsCurrent(true);
         } else if (ip.equals(network.getGateway())) {
